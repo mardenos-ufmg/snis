293b6a72df08044c0386f65b81042e191eb464a0
Carlos de Moura <lcbarrosdemoura@gmail.com>
2025-10-25 09:57:55 -0300

    mudanças em get_data; melhora no readme; inicio das vignettes

--- R/get_data.R
+++ R/get_data.R
@@ -1,16 +1,18 @@
-
-####################
-####  ler base  ####
-####################
-
-read = function(year) {
-  stopifnot("Ano deve estar entre 2010 e 2022" = year %in% 2010:2022)
-  # dados retirados de https://app4.cidades.gov.br/serieHistorica/
+#' Ler CSV
+#'
+#' @param ano integer between 2010&
+#'
+#' @returns tibble::tibble
+#'
+#' @export
+#'
+# dados retirados de https://app4.cidades.gov.br/serieHistorica/
+read = function(ano) {
+  stopifnot("Ano deve estar entre 2015 e 2022" = ano %in% 2010:2022)
 
   ibge =
-    readxl::read_xls("data/RELATORIO_DTB_BRASIL_2024_MUNICIPIOS.xls", skip = 6) |>
-    #read.csv(file("data/RELATORIO_DTB_BRASIL_2024_MUNICIPIOS.ods", encoding = "ASCII")) |>
-    dplyr::select(c("Código Município Completo",
+    readODS::read_ods("data/RELATORIO_DTB_BRASIL_2024_MUNICIPIOS.ods", skip = 6) |>
+    select(c("Código Município Completo",
                     "Região Geográfica Intermediária",
                     "Nome Região Geográfica Intermediária",
                     "Região Geográfica Imediata",
@@ -20,86 +22,96 @@ read = function(year) {
                    "Região Intermediária",
                    "Código da Região Imediata",
                    "Região Imediata")) |>
-    dplyr::mutate(
-      prefix = substr(.data$"Código do Município", 1, 6)
+    mutate(
+      prefix = substr(.data$"Código do Município", 1, 6) |> as.numeric(),
+      across(starts_with("Código"), as.numeric)
     ) |>
     suppressWarnings() |>
     suppressMessages()
 
+  colnames =
+    paste0("data/Desagregado-", ano, ".csv") |>
+    file(encoding = "UTF-16LE") |>
+    readLines(1) |>
+    strsplit(";") |>
+    purrr::pluck(1) |>
+    {\(.) gsub("\"", "", .)}() |>
+    {\(.) gsub("\\\\", "", .)}() |>
+    {\(.) sub(" - .*", "", .) }()
+
   df =
-    paste0("data/Desagregado-", year, ".csv") |>
+    paste0("data/Desagregado-", ano, ".csv") |>
     file(encoding = "UTF-16LE") |>
     readLines() |>
-    {\(.) sub(";\\s*$", "", .)}() |>
+    {\(.) .[2:(length(.)-1)] }() |>
+    {\(.) substr(., 1, nchar(.) - 1) }() |>
     {\(.)
       read.csv2(
         text = .,
+        header = FALSE,
         dec = ",",
-        header = TRUE,
         stringsAsFactors = FALSE,
-        check.names = FALSE
-        )
+        row.names = NULL
+      )
     }() |>
-    {\(.) `colnames<-`(., sub(" - .*", "", colnames(.) ))}() |>
     tibble::as_tibble() |>
-    {\(.) dplyr::mutate(., across(where(is.character),
-                                  ~ na_if(., "")))
-      }() |>
+    `colnames<-`(colnames) |>
+    {\(.) mutate(.,
+                        across(
+                          where(is.character),
+                          ~ na_if(., "")
+    ))}() |>
     {\(.) {
-      if(  grepl("TOTAL DA AMOSTRA", toupper(tail(.[[1]], 1)))  ) {
-        . = head(., -1)
-      }
-      .
-    }}() |>
-    {\(.) {
-      col_sem_letras = names(.)[
-        sapply(., function(col) is.character(col) && all(!grepl("[A-Za-z]", col)))
-      ]
-      col_sem_letras = setdiff(col_sem_letras, c("Código do Prestador", "Código do Município"))
+      col_sem_letras = names(.)[  sapply(., function(col) is.character(col) && all(!grepl("[A-Za-z]", col)))  ]
       mutate(., across(all_of(col_sem_letras),
                        ~ readr::parse_number(., locale = readr::locale(decimal_mark = ",", grouping_mark = "."))
-                       ))
+      ))
     }}() |>
     mutate(across(where(is.character), stringr::str_trim)) |>
-    dplyr::left_join(ibge, by = c("Código do Município" = "prefix")) |>
-    dplyr::select(-all_of("Código do Município")) |>
-    dplyr::rename("Código do Município" = "Código do Município.y") |>
-    dplyr::relocate("Código do Município") |>
-    dplyr::relocate(c("Código da Região Intermediária", "Região Intermediária", "Código da Região Imediata", "Região Imediata"), .after = "Natureza jurídica") |>
-    dplyr::relocate(c("POP_TOT", "POP_URB"), .after = "Região Imediata")
-
-  colnames(df)[1:14] = tolower(colnames(df)[1:14])
+    left_join(ibge, by = c("Código do Município" = "prefix")) |>
+    select(-all_of("Código do Município")) |>
+    rename("Código do Município" = "Código do Município.y") |>
+    relocate("Código do Município") |>
+    relocate(c("Código da Região Intermediária", "Região Intermediária", "Código da Região Imediata", "Região Imediata"), .after = "Natureza jurídica") |>
+    relocate(c("POP_TOT", "POP_URB"), .after = "Região Imediata") |>
+    rename_with(tolower, .cols = 1:14) |>
+    mutate(
+      across(c("prestador", "sigla do prestador", "abrangência", "tipo de serviço", "natureza jurídica", "região intermediária","região imediata"),
+             as.factor)
+    )
 
   df
 }
 
 
-#################################
-####  tratar para aplicação  ####
-#################################
 
-process_data = function(year) {
+#' Processar dados
+#'
+#' @param ano inteiro
+#'
+#' @returns tibble::tibble
+#' @export
+process_data = function(ano) {
+  cols = c(
+    "ano de referência","natureza jurídica","tipo de serviço","abrangência","município",
+    "código do município","código da região intermediária", "região intermediária",
+    "código do prestador","prestador","POP_URB","POP_TOT",
+    "IN002","IN031","IN101","IN049","IN019","IN023","IN024",
+    "IN055","IN056","IN057","IN075","IN076","IN084","IN046",
+    "IN015","IN009","IN013","IN029","IN058","IN004","IN003",
+    "AG013","AG022","IN006","IN016","IN047","ES006","ES005"
+  )
+
   df =
-    read(year) |>
-    dplyr::select(
-      all_of(c(
-        "natureza jurídica","tipo de serviço","abrangência","município", "código do município",
-        "código da região intermediária", "região intermediária", "código do prestador","prestador","POP_URB","POP_TOT",
-        "IN002","IN031","IN101","IN049","IN019","IN023","IN024",
-        "IN055","IN056","IN057","IN075","IN076","IN084","IN046",
-        "IN015","IN009","IN013","IN029","IN058","IN004","IN003",
-        "AG013","AG022","IN006","IN016","IN047","ES006","ES005"
-      ))
-    ) |>
-    dplyr::filter(.data$"tipo de serviço" != "Esgotos") |>
+    read(ano) |>
+    select(all_of(cols)) |>
+    filter(.data$"tipo de serviço" != "Esgotos") |>
     mutate(
       across(c("IN006","IN016","IN047","IN015"),
              ~ case_when(
                is.na(.) & .data$`tipo de serviço` == "Água" ~ 0,
                TRUE ~ .
-             )
-      )
-    )
+    )))
 
   #IN024 tem multicolinearidade com IN047
   #AG022 tem multicolineadidade com AG013
@@ -129,8 +141,8 @@ process_data = function(year) {
       urbanização = .data$POP_URB / .data$POP_TOT
     )
 
-  # qtde_n_micromedida  VIROU Micromedida
-  # grau_urbanizacao    VIROU Urbanização
+  # qtde_n_micromedida  VIROU micromedida
+  # grau_urbanizacao    VIROU urbanização
 
   df =
     df[,!(names(df) %in% numerico)] |>
@@ -165,7 +177,7 @@ process_data = function(year) {
 
   df =
     df |>
-    dplyr::filter(!(.data$município %in% municipios_duplicados$município)) |>
+    filter(!(.data$município %in% municipios_duplicados$município)) |>
     rbind(municipios_filter) |>
     tibble::as_tibble() |>
     relocate(c("tarifa", "micromedida", "urbanização", "prestador2"))
